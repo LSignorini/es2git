20df3dce63ec5d8f148affba71ee391552e2238d
Luca Signorini - luca.signorini12@studio.unibo.it <luca.signorini12@studio.unibo.it>
2022-10-07 16:27:50 +0200

    change fixing mode

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,16 +1,16 @@
 public final class HelloWorld {
 
-	static final String AUTHOR = "Danilo Pianini";
+	private static final String AUTHOR = "Danilo Pianini";
 
 	public static void main(final String[] args) {
 <<<<<<< HEAD
 		System.out.println("This program is running in a PC with " + procNumber() + " logic processors!");
+		System.out.println("This program has been realised by " + AUTHOR);
 	}
 
 	public static int procNumber() {
 		return Runtime.getRuntime().availableProcessors();
 =======
-		System.out.println("This program has been realised by " + AUTHOR);
 >>>>>>> feature
 	}
 
